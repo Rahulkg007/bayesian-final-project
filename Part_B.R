94506247178bb3df8a8cb341f9df1bbdd8d47ead
Carlos Castillo <carloscastillo@Carloss-MacBook-Air.local>
2018-09-30 20:44:01 +1000

    Model Up Running

--- Part_B.R
+++ Part_B.R
@@ -4,8 +4,10 @@
 graphics.off() # This closes all of R's graphics windows.
 #rm(list=ls())  # Careful! This clears all of R's memory!
 setwd("/Users/carloscastillo/Documents/RMIT/Semester 4/Bayesian Statistics/Project/bayesian-final-project")
-
+source("Final Project.R")
 ##myData = read.csv("Assignment2PropertyPrices.csv")
+myData = data.merge
+
 yName = "count" ; xName = c("Total_Persons_Persons",	"Median_age_persons",	"Median_mortgage_repay_monthly", 	"Median_tot_hhd_inc_weekly",  	"Average_household_size","P_PGrad_Deg_Total","P_GradDip_and_GradCert_Total" , "P_BachDeg_Total","Certificates","Not Sated","Not Sated","Prv_Trs","Non_Motor_Trs")
 #yName = "SalePrice" ; xName = c("Area",	"Bedrooms",	"Bathrooms", 	"CarParks")
 
@@ -19,7 +21,7 @@ source("Part_B_Info.R")
 #------------------------------------------------------------------------------- 
 # Generate the MCMC chain:
 #startTime = proc.time()
-xPred = c(250 ,	3 ,	2 ,1 , 0 )
+xPred = c(8845,26,2000,1230,2.4,901,93,2538,1405,1176,1005,588,1370 )
 #xPred = c(600 ,	2 ,	2 ,	1  )
 colnames(xPred) = c("beta[1]","beta[2]","beta[3]","beta[4]","beta[5]","beta[6]","beta[7]","beta[8]","beta[9]","beta[10]","beta[11]","beta[12]","beta[13]")
 #colnames(xPred) = c("beta[1]","beta[2]","beta[3]","beta[4]")
